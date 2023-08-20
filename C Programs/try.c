fb6b2f82da2a0d3e052255eef8fd7c576ce11d6e
EH.Milon <ehasanulhaquemilon@gmail,com>
2023-08-21 00:12:21 +0600

    changes

--- C Programs/try.c
+++ C Programs/try.c
@@ -1,7 +1,15 @@
-/* Program Name: */
+/* Example */
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 int main() {
-  printf("%d", -25 / 9);
+  int a, b, c, d, e;
+  printf("Enter a and b: \n");
+  scanf("%d %d", &a, &b);
+  c = (a - b) * 2;
+  e = a / (b + 1);
+  d = e + a;
+  printf("%d %d %d\n", c, d, e);
 
   return 0;
 }
