8b58c2e85d22645da189a72c5bd7e1f792391638
EH.Milon <ehasanulhaquemilon@gmail,com>
2023-09-20 00:38:41 +0600

    update

--- C Programs/try.c
+++ C Programs/try.c
@@ -1,15 +1,7 @@
-/* Example */
+/* prime number */
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 int main() {
-  int a, b, c, d, e;
-  printf("Enter a and b: \n");
-  scanf("%d %d", &a, &b);
-  c = (a - b) * 2;
-  e = a / (b + 1);
-  d = e + a;
-  printf("%d %d %d\n", c, d, e);
 
+  printf("Hello World!");
   return 0;
 }
